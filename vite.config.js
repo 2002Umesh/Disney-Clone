994bf0ee790a25a6819f7598103a2c6fffb62d8a
Umesh Das <dasumesh2002@gmail.com>
2024-01-29 22:39:59 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createRouter } from "vue-router";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    createVuePlugin(),
-    {
-      configureServer: [async ({ app }) => {
-        const { default: routes } = await import('./routes') // Import your routes configuration
-        const router = createRouter({
-          history: createWebHistory(),
-          routes,
-        })
-        app.use(router)
-      }],
-    },
-  ],
-
+  plugins: [react()],
 })
