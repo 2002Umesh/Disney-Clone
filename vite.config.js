b9e7ebae2749208afb2bef64b07a2269ea6c41e9
Umesh Das <dasumesh2002@gmail.com>
2024-01-29 22:11:57 +0530

    done 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { createVuePlugin } from "vite-plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createVuePlugin(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+      rewrite: (path) => path.replace(/^\/old-path/, "/new-path"),
+    }),
+  ],
+});
